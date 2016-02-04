8cbf08a8a9c01c167b2417be4bf28fd7437399c8
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-02-04 11:49:23 +0200

    settings change

--- DbContext/settings.py
+++ DbContext/settings.py
@@ -1,2 +1,2 @@
-XML_FILES_LOCATION = ''
-TEXT_FILES_LOCATION = ''
+XML_FILES_LOCATION = '/media/cs/my drive/citeseerx/citeseerx_xml_9_15_2015.tar.gz'
+TEXT_FILES_LOCATION = '/media/cs/my drive/citeseerx/citeseerx_text_4_21_2015.tar.gz'
